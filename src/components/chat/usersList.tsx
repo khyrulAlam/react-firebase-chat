d45e50885d1ca3f74980338d1167cdd0ca8d6ad0
khyrulAlam <khyrulalam69@gmail.com>
2025-01-13 19:13:05 +0900

    User serach feature

--- src/components/chat/usersList.tsx
+++ src/components/chat/usersList.tsx
@@ -8,30 +8,27 @@ import { ChatActionType } from "@/context/Chat/types";
 import { unSubscribeDatabase } from "@/services";
 import { firebaseDatabase } from "@/config";
 import { onValue, ref } from "firebase/database";
-import { numberFormatter } from "@/utils";
+import { DB_NAME, numberFormatter } from "@/utils";
+import SearchBox from "./searchBox";
 
 const UsersList = () => {
   const [loading, setLoading] = useState(true);
   const { user: authUser } = useAuth();
   const { chatRoomId, oneToOneRoomId } = useChat();
   const dispatch = useChatDispatch();
+  const { userList: storeUserList } = useChat();
   const [userList, setUserList] = useState<User[] | []>([]);
   const [userCount, setUserCount] = useState<number>(0);
+  const [startSearch, setStartSearch] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    const collectionRef = ref(firebaseDatabase, "usersTable");
+    const collectionRef = ref(firebaseDatabase, DB_NAME.USER_TABLE);
     const unsubscribe = onValue(collectionRef, (snapshot) => {
       const data = snapshot.val();
       if (data) {
         const users = Object.values(data) as User[];
         setUserCount(users.length || 0);
-        // Remove duplicate users and the current user
-        const uniqueUsers = users.filter(
-          (user, index, self) =>
-            user.uid && user.uid !== authUser?.uid && index === self.findIndex((u) => u.uid === user.uid)
-        );
-        setUserList(uniqueUsers);
         dispatch({
           type: ChatActionType.SET_USER_LIST,
           payload: data,
@@ -45,11 +42,13 @@ const UsersList = () => {
     };
   }, [authUser?.uid, dispatch]);
 
-  const handleUserClick = (user: User | "chatRoom") => {
+  const handleUserClick = (user: User | string) => {
     const newChatRoomId =
-      user === "chatRoom" ? "chatRoom" : `${authUser?.uid}+${user.uid}`;
+      typeof user === "string"
+        ? DB_NAME.CHAT_ROOM
+        : `${authUser?.uid}+${user.uid}`;
     const newOneToOneRoomId =
-      user !== "chatRoom" ? `${user.uid}+${authUser?.uid}` : "";
+      typeof user !== "string" ? `${user.uid}+${authUser?.uid}` : "";
 
     // Unsubscribe from the database
     const roomList = [chatRoomId, oneToOneRoomId].filter(Boolean);
@@ -65,6 +64,16 @@ const UsersList = () => {
     });
   };
 
+  const handleSearchUserList = (token: string) => {
+    const sourceList = Object.values(storeUserList) as User[];
+    const regex = new RegExp(token, "i");
+    const result = sourceList
+      .filter((user) => user.userName.toLowerCase().match(regex))
+      .sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1));
+    setUserList(result);
+    setStartSearch(false);
+  };
+
   return (
     <Box
       p={2}
@@ -78,15 +87,19 @@ const UsersList = () => {
       {loading && <Spinner />}
       {!loading && (
         <Stack gap="2">
+          <SearchBox
+            setSearch={handleSearchUserList}
+            setLoading={setStartSearch}
+          />
           <HStack
             cursor={"pointer"}
             _hover={{ bg: "orange.50", color: "orange.400" }}
-            bg={chatRoomId === "chatRoom" ? "orange.50" : "gray.subtle"}
+            bg={chatRoomId === DB_NAME.CHAT_ROOM ? "orange.50" : "gray.subtle"}
             color={"orange.400"}
             borderRadius={"md"}
             title="Common Group"
             p={"2"}
-            onClick={() => handleUserClick("chatRoom")}
+            onClick={() => handleUserClick(DB_NAME.CHAT_ROOM)}
           >
             <AvatarGroup size="sm">
               <Avatar
@@ -94,45 +107,61 @@ const UsersList = () => {
                 src={authUser?.profile_picture}
                 colorPalette={"orange"}
               />
-              <Avatar colorPalette={'orange'} fallback={`+${numberFormatter.format(userCount)}`} />
+              <Avatar
+                colorPalette={"orange"}
+                fallback={`+${numberFormatter.format(userCount)}`}
+              />
             </AvatarGroup>
             <Text fontWeight="medium" textStyle={"sm"} lineClamp={1}>
               Common Group
             </Text>
           </HStack>
-          {userList?.map((user) => (
-            <HStack
-              key={user.uid}
-              cursor={"pointer"}
-              bg={
-                chatRoomId === `${authUser?.uid}+${user.uid}`
-                  ? "orange.50"
-                  : "gray.subtle"
-              }
-              color={
-                chatRoomId === `${authUser?.uid}+${user.uid}`
-                  ? "orange.400"
-                  : "initial"
-              }
-              _hover={{ bg: "orange.50", color: "orange.400" }}
-              borderRadius={"md"}
-              title={user.userName}
-              p={"2"}
-              onClick={() => handleUserClick(user)}
-            >
-              <Avatar
-                name={user.userName}
-                size="sm"
-                src={user.profile_picture}
-                captionSide={"bottom"}
-              />
-              <Stack gap="0">
-                <Text fontWeight="medium" textStyle={"sm"} lineClamp={1}>
-                  {user.userName}
-                </Text>
-              </Stack>
+
+          {startSearch ? (
+            <Stack alignItems={"center"}>
+              <Spinner />
+            </Stack>
+          ) : userList.length < 1 ? (
+            <HStack justifyContent={"center"}>
+              <Text fontWeight="medium" textStyle={"xs"}>
+                No Search result found
+              </Text>
             </HStack>
-          ))}
+          ) : (
+            userList?.map((user) => (
+              <HStack
+                key={user.uid}
+                cursor={"pointer"}
+                bg={
+                  chatRoomId === `${authUser?.uid}+${user.uid}`
+                    ? "orange.50"
+                    : "gray.subtle"
+                }
+                color={
+                  chatRoomId === `${authUser?.uid}+${user.uid}`
+                    ? "orange.400"
+                    : "initial"
+                }
+                _hover={{ bg: "orange.50", color: "orange.400" }}
+                borderRadius={"md"}
+                title={user.userName}
+                p={"2"}
+                onClick={() => handleUserClick(user)}
+              >
+                <Avatar
+                  name={user.userName}
+                  size="sm"
+                  src={user.profile_picture}
+                  captionSide={"bottom"}
+                />
+                <Stack gap="1" direction={"column"} align={"flex-start"}>
+                  <Text fontWeight="medium" textStyle={"sm"} lineClamp={1}>
+                    {user.userName}
+                  </Text>
+                </Stack>
+              </HStack>
+            ))
+          )}
         </Stack>
       )}
     </Box>
---
+++ src/utils/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+const useDebounce = <T>(value: T, delay = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
